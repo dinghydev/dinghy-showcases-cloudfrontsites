eb62c7bfd3c1875ad22859875f6fac58f5a5f69a
Guo Du <mrduguo@gmail.com>
2026-02-14 15:18:38 +0000

    Update to cloudfront namespace

--- dinghy-dev-demo-sites.tsx
+++ dinghy-dev-demo-sites.tsx
@@ -1,6 +1,6 @@
 import { MoveToHere } from '@dinghy/base-components'
 import { AwsStack } from '@dinghy/tf-aws'
-import { CloudfrontSites } from '@dinghy/tf-aws/cloudfrontSites'
+import { CloudfrontSites } from '@dinghy/tf-aws/cloudfront'
 
 export default () => (
   <AwsStack infrastructure={<MoveToHere includes='AwsRoute53Zone' />}>
