801d8261637ab20b15af8067c4e62abf3c4fe53e
Guo Du <mrduguo@gmail.com>
2026-06-21 14:40:06 +0100

    Upgrade to support create Route53Zone

--- dinghy-dev-demo-sites.tsx
+++ dinghy-dev-demo-sites.tsx
@@ -1,9 +1,8 @@
-import { MoveToHere } from '@dinghy/base-components'
 import { AwsStack } from '@dinghy/tf-aws'
 import { CloudfrontSites } from '@dinghy/tf-aws/cloudfront'
 
 export default () => (
-  <AwsStack infrastructure={<MoveToHere includes='AwsRoute53Zone' />}>
+  <AwsStack>
     <CloudfrontSites />
   </AwsStack>
 )
